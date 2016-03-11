92b52374ac75225d0f0be0900dd5426cba1b50f9
isennett <isabelle.sennett@tufts.edu>
2016-03-10 23:06:07 -0500

    Part 1 done

---
+++ messages/README.md
@@ -0,0 +1,6 @@
+Isabelle Sennett  
+March 10, 2016  
+Lab 6: Messages
+
+
+Time spent: 
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here...
+// Isabelle Sennett -- comp20 -- March 2016
+
+function parse(){
+    request = new XMLHttpRequest();
+    request.open("GET", "data.json", true);
+    request.onreadystatechange = callme;
+    request.send(null);
+};
+
+function callme () {
+    if (request.readyState == 4 && request.status == 200) {
+        result = "";
+        raw = request.responseText;
+        theMessage = JSON.parse(raw);
+        elem = document.getElementById("messages");
+        for (i = 0; i < theMessage.length; i++) {
+            result += "<p>" + theMessage[i]["content"] + " " + theMessage[i]["username"]+ "</p>";
+        }
+        elem.innerHTML = result;
+    }
+    else if (request.readyState == 4 && request.status != 200) {
+        document.getElementById("messages").innerHTML = "<p>Whoops, #fail</p>";
+    }
+};
